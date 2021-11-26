467361a1535afb271de99d5643f6dd318b7416ba
udogaL2 <geka039-haker@yandex.ru>
2021-11-27 00:20:28 +0200

    Добавлено: спрайт, хитбокс, застревание в стенах, заготовка для игрового поля

--- main.cpp
+++ main.cpp
@@ -14,35 +14,62 @@ const double PI = acos(-1);
 
 using namespace std::chrono_literals;
 
-class Circle {
+float getAngle(float x0, float y0, float x1, float y1) {
+    float dy = y1 - y0;
+    float dx = x1 - x0;
+    float angle = atan(dy / dx);
+    if (dx < 0)
+        angle += acos(-1);
+    if (dx == 0 and dy == 0) {
+        angle = 0;
+    }
+    return angle;
+}
+
+class Dart {
 public:
-    Circle(float x, float y, float R, sf::Color Color, float angle, float v0) {
+    Dart(float x, float y, float w, float h, float angle, float v0) {
         s_y = y;
         s_x = x;
         s_x0 = x;
         s_y0 = y;
-        radius = R;
-        color = Color;
+        s_w = w;
+        s_h = h;
         s_angle = angle;
         s_v0 = v0;
         s_t = 0;
-        m_shape = new sf::CircleShape(radius);
-        m_shape->setFillColor(color);
-        m_shape->setOrigin(radius, radius);
+        uploadTexture();
+        m_shape = new sf::Sprite();
+        heat_box = new sf::RectangleShape(sf::Vector2f(2 * s_w, 2 * s_h));
+        m_shape->setTexture(m_texture);
+//        m_shape->setScale(0.4, 0.4);
+        m_shape->setOrigin(s_w, s_h);
+        heat_box->setOrigin(s_w, s_h);
         m_shape->setPosition(s_x, s_y);
+        heat_box->setPosition(s_x, s_y);
     }
 
-    ~Circle() {
+    ~Dart() {
         delete m_shape;
     }
 
-    sf::CircleShape *getShape() {
+    bool uploadTexture() {
+        if (!m_texture.loadFromFile("../img/darts.png")) {
+            std::cout << "ERROR when loading bird.png" << std::endl;
+            return false;
+        }
+        m_texture.setSmooth(true);
+        return true;
+    }
+
+    sf::Sprite *getShape() {
         return m_shape;
     }
 
     void setStartPosition(float x0, float y0) {
         s_x0 = x0;
         s_y0 = y0;
+        m_shape->setRotation(0);
     }
 
     void setAngle(float angle) {
@@ -53,39 +80,50 @@ public:
         s_v0 = v0;
     }
 
-    void setSelfTime(float time){
+    void setSelfTime(float time) {
         s_t = time;
     }
 
-    void addSelfTime(float time){
+    void addSelfTime(float time) {
         s_t += time;
     }
 
     void setPosition(float x, float y) {
-        if (x - radius <= 0)
-            s_x = radius;
-        else if (x + radius > WIDTH)
-            s_x = WIDTH - radius;
+        if (x - s_w + 30 < 0)
+            s_x = s_w - 30;
+        else if (x + s_w - 30 > WIDTH)
+            s_x = WIDTH - s_w + 30;
         else
             s_x = x;
-        if (y - radius < 0)
-            s_y = radius;
-        else if (y + radius > HEIGHT)
-            s_y = HEIGHT - radius;
+        if (y - s_h < 0)
+            s_y = s_h;
+        else if (y + s_h > HEIGHT)
+            s_y = HEIGHT - s_h;
         else
             s_y = y;
         m_shape->setPosition(s_x, s_y);
+        heat_box->setPosition(s_x, s_y);
     }
 
     void Move() {
-        if (s_x + radius < WIDTH) {
+        if (s_x + s_w - 30 >= WIDTH) {
+            // функция, которая добавляет очки
+            std::cout << "add points" << '\n';
+            std::this_thread::sleep_for(500ms);
+            this->s_v0 = 5;
+            this->s_angle = PI / 2;
+            this->setPosition(250, 150);
+            this->setStartPosition(250, 150);
+            s_t = 0;
+        } else if (s_x - s_w + 30 > 0 and s_y - s_h > 0 and s_y + s_h < HEIGHT) {
             float x = s_x0 + s_v0 * cos(s_angle) * s_t;
             float y = s_y0 + s_v0 * sin(s_angle) * s_t + G * s_t * s_t / 2;
             this->setPosition(x, y);
+            m_shape->setRotation(::getAngle(s_x0, s_y0, s_x, s_y) * (180.0 / PI));
         }
         else{
-            // функция, которая добавляет очки
-            std::cout << "add points" << '\n';
+            // отнятие жизни у игрока
+            std::cout << "remove 1HP" << '\n';
             std::this_thread::sleep_for(500ms);
             this->s_v0 = 5;
             this->s_angle = PI / 2;
@@ -103,12 +141,21 @@ public:
         return s_y;
     }
 
-    float getRadius() {
-        return radius;
+    float getHeight() {
+        return s_h;
+    }
+
+    float getWidth() {
+        return s_w;
     }
 
     float getAngle() {
         return s_angle;
+    }
+
+    void deleteMyself(){
+        delete m_shape;
+        delete heat_box;
     }
 
 private:
@@ -116,89 +163,88 @@ private:
     float s_y;
     float s_x0;
     float s_y0;
-    float radius;
+    float s_h;
+    float s_w;
     float s_angle;
     float s_v0;
     float s_t;
 
-    sf::CircleShape *m_shape;
-    sf::Color color;
+
+    sf::Texture m_texture;
+    sf::Sprite *m_shape;
+    sf::RectangleShape *heat_box;
 };
 
-void update(sf::RenderWindow &window, Circle &circle) {
+void update(sf::RenderWindow &window, Dart &circle) {
     window.clear();
     window.draw(*circle.getShape());
     window.display();
 }
 
-bool moveWhenPush(sf::RenderWindow &window, Circle &circle) {
+bool moveWhenPush(sf::RenderWindow &window, Dart &dart) {
     sf::Vector2i mouse_position = sf::Mouse::getPosition(window);
-    if (pow((circle.getX() - mouse_position.x), 2) + pow((circle.getY() - mouse_position.y), 2) <=
-        pow(circle.getRadius(), 2)) {
+    if ((dart.getX() + dart.getWidth() >= mouse_position.x and dart.getX() - dart.getWidth() <= mouse_position.x) and
+        (dart.getY() + dart.getHeight() >= mouse_position.y and dart.getY() - dart.getHeight() <= mouse_position.y)) {
         while (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
             mouse_position = sf::Mouse::getPosition(window);
-            circle.setPosition(mouse_position.x, mouse_position.y);
-            update(window, circle);
+//            if (mouse_position.x <= 500)
+                dart.setPosition(mouse_position.x, mouse_position.y);
+//            else
+//                sf::Mouse::setPosition(sf::Vector2i(500, mouse_position.y), window);
+            update(window, dart);
         }
         return true;
     }
     return false;
 }
 
+float getDistance(float x0, float y0, float x1, float y1) {
+    return sqrt(pow((x1 - x0), 2) + pow(y1 - y0, 2));
+}
+
 int main() {
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SFML_Project", sf::Style::Close);
     window.setFramerateLimit(120);
-    Circle circle(100., 80., 20, sf::Color::Blue, PI/2, 20);
+    Dart dart(100., 80., 102 / 2, 25 / 2, PI / 2, 20);
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
-                delete circle.getShape();
+                dart.deleteMyself();
             }
         }
 
 
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-            if (moveWhenPush(window, circle)) {
+            if (moveWhenPush(window, dart)) {
                 std::this_thread::sleep_for(40ms);
                 sf::Vector2i mouse_position = sf::Mouse::getPosition(window);
-                float x0 = circle.getX();
-                float y0 = circle.getY();
+                float x0 = dart.getX();
+                float y0 = dart.getY();
                 int x1 = mouse_position.x;
                 int y1 = mouse_position.y;
 
-                float d = sqrt(pow((x1 - x0), 2) + pow(y1 - y0, 2));
-                float dy = y1 - y0;
-                float dx = x1 - x0;
+                dart.setStartPosition(dart.getX(), dart.getY());
 
-                float angle = atan(dy / dx);
-                if (dx < 0)
-                    angle += PI;
+                dart.setInitialSpeed(getDistance(x0, y0, x1, y1));
+                dart.setAngle(getAngle(x0, y0, x1, y1));
 
-                if (dx == 0 and dy == 0) {
-                    angle = 0;
-                    d = 0;
-                }
-                circle.setStartPosition(circle.getX(), circle.getY());
-                circle.setInitialSpeed(d);
-                circle.setAngle(angle);
-                std::cout << circle.getAngle() << '\n';
-                circle.setSelfTime(0);
+                dart.setSelfTime(0);
             }
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
             window.close();
-            delete circle.getShape();
+            dart.deleteMyself();
         }
 
-        circle.Move();
+        dart.Move();
 
-        update(window, circle);
+        update(window, dart);
 //        std::this_thread::sleep_for(10ms);
-        circle.addSelfTime(D_T);
+        dart.addSelfTime(D_T);
     }
 
     return 0;
